9c449c73b60c82cb37f95a5c97fe168bded69f76
Islem Khemissi <khemissi.islem@gmail.com>
2023-08-30 19:33:57 -0400

    home

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,15 @@
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className='hidden xl:flex xl:max-w-none'>
+    <Image
+      alt=''
+      width={737}
+      height={678}
+      src={'/avatar.png'} 
+      className='translate-z-0 w-full h-full'
+    />
+  </div>;
 };
 
 export default Avatar;
--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { HiArrowRight } from "react-icons/hi2"
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className='mx-auto xl:mx-0'>
+      <Link href={'/work'} className='relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'>
+        <Image 
+          alt='' 
+          width={141} 
+          height={148}
+          src={'/rounded-text.png'}
+          className='animate-spin-slow w-full w-max-[141px] h-max-[148px]'
+        />
+        <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300' />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
